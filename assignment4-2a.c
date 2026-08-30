8cba664c669d494c7e12b34f88c354d95b326613
Manas <manas@manass-MacBook-Air-2.local>
2026-08-31 00:33:13 +0530

    Add assignment4-2a C program

---
+++ assignment4-2a.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() 
+{
+    int a;
+    float b, c;
+    printf("Enter a integer number: ");
+    scanf("%d", &a);
+    printf("Enter an floating-point number: ");
+    scanf("%f", &b);    
+    printf("\nImplicit Type Conversion\n");
+    
+    printf("Addition = %.2f\n", c);
+    c = a + b;
+
+    c = a - b;
+    printf("Subtraction = %.2f\n", c);
+    
+    c = a * b;
+    printf("Multiplication = %.2f\n", c);
+    
+    c = a / b;
+    printf("Division = %.2f\n", c);
+    return 0;
+}
